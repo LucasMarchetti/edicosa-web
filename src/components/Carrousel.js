1ce9196f4c0243dd77ee4be35ee4782128108310
LucasMarchetti <luksmarchetti@gmail.com>
2024-07-01 12:21:50 -0300

    Fix Carrousel.js bug

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -1,38 +1,32 @@
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './Carrousel.css';
 
 // Images
-import image1 from "../images/carrousel/1.webp"
-import image2 from "../images/carrousel/2.jpg"
-import image3 from "../images/carrousel/3.avif"
-
+import image1 from "../images/carrousel/1.webp";
+import image2 from "../images/carrousel/2.jpg";
+import image3 from "../images/carrousel/3.avif";
 
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const images = [
-    image1,
-    image2,
-    image3,
-  ]
+  const images = [image1, image2, image3];
 
-  const nextSlide = () => {
+  const nextSlide = useCallback(() => {
     const newIndex = (currentIndex + 1) % images.length;
     setCurrentIndex(newIndex);
-  };
+  }, [currentIndex, images.length]);
 
-  const prevSlide = () => {
+  const prevSlide = useCallback(() => {
     const newIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
-  };
+  }, [currentIndex, images.length]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       nextSlide();
     }, 5000);
     return () => clearInterval(interval);
-  }, [nextSlide, currentIndex]);
+  }, [nextSlide]);
 
   return (
     <div className="carousel">
